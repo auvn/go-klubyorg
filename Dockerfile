e91acccd541bc0cdeb19c5d0435becd154f48c46
Dmitry Marshkov <dmarshkov@faraway.gg>
2025-06-26 22:22:15 +0200

    Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM golang:1.24-bullseye AS builder
+
+WORKDIR /app
+
+COPY ./go.mod ./go.sum ./
+RUN go mod download
+
+COPY . .
+
+RUN go build -trimpath -ldflags="-w -s" -o /bin/service ./cmd/klubyorg/...
+
+FROM gcr.io/distroless/base-debian11:nonroot
+USER nonroot:nonroot
+WORKDIR /
+COPY --from=builder /bin/sleep /bin/sleep
+COPY --from=builder /bin/service /bin/service
+
+CMD ["/bin/service"]
--- cmd/klubyorg/main.go
+++ cmd/klubyorg/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"context"
-	_ "embed"
 	"net/http"
 
 	"github.com/auvn/go-app/bootstrap/appx"
 	"github.com/auvn/go-app/httpx"
-	"github.com/auvn/go-klubyorg/internal/api/connect/klubyorg/v1api"
+	klubyorgv1api "github.com/auvn/go-klubyorg/internal/api/connect/klubyorg/v1api"
 	"github.com/auvn/go-klubyorg/internal/service/klubyorg"
 	"github.com/auvn/go-klubyorg/pkg/gen/proto/klubyorg/v1/v1connect"
 )
@@ -18,7 +17,7 @@ func main() {
 	app.Go(func(ctx context.Context) error {
 		var mux http.ServeMux
 
-		courtsServiceHandler := v1api.NewHandler(klubyorg.NewService())
+		courtsServiceHandler := klubyorgv1api.NewHandler(klubyorg.NewService())
 
 		mux.Handle(v1connect.NewGetCourtsServiceHandler(courtsServiceHandler))
 
